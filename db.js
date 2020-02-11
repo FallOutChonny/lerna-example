607812e71cef6b3fd4b9f45a8efc2622c0a7b4fe
Chonny <mama.whowho@gmail.com>
2020-02-11 11:52:41 +0800

    feat: add mock server

---
+++ db.js
@@ -0,0 +1,20 @@
+module.exports = () => {
+  let data = {
+    users: [
+      {
+        id: 1,
+        name: 'chonny',
+      },
+      {
+        id: 2,
+        name: 'mary',
+      },
+      {
+        id: 3,
+        name: 'danny',
+      },
+    ],
+  }
+
+  return data
+}
